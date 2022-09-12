6369016d4ee57d84369eedc8143d8d0324d5c57a
ArshvirSk <arshvirsk26@gmail.com>
2022-09-12 23:55:37 +0530

    first commit

---
+++ client/build/precache-manifest.174b2fcf3e0cd0138c8c7ac7ea1b3c04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "866613514f5350628d01",
+    "url": "/static/css/main.f5e2c320.chunk.css"
+  },
+  {
+    "revision": "866613514f5350628d01",
+    "url": "/static/js/main.86661351.chunk.js"
+  },
+  {
+    "revision": "d25426de787fb0aa38d9",
+    "url": "/static/js/1.d25426de.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3889b42d1d76c10d789307a4bcb30d29",
+    "url": "/index.html"
+  }
+];
---
+++ client/src/App.js
@@ -0,0 +1,152 @@
+import React from "react";
+import { useState } from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import io from "socket.io-client";
+import Chat from "./Chat";
+import "./App.css";
+
+// const socket = io.connect("https://floating-garden-45600.herokuapp.com/");
+const socket = io.connect("http://localhost:3001");
+
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+      localStorage.setItem('username', username);
+    }
+  };
+
+  const particlesInit = async (main) => {
+    console.log(main);
+    await loadFull(main);
+  };
+  
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  return (
+    <div className="App">
+      <img src="./logo.png" alt="" />
+      <Particles className="particles"
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={{
+              fpsLimit: 60,
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: true,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: true,
+                    mode: "repulse",
+                  },
+                  resize: true,
+                },
+                modes: {
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 200,
+                    duration: 0.4,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: "#ff0000",
+                },
+                links: {
+                  color: "#ff0000",
+                  distance: 200,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 1,
+                },
+                collisions: {
+                  enable: true,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outModes: {
+                    default: "bounce",
+                  },
+                  random: false,
+                  speed: 2,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                    area: 800,
+                  },
+                  value: 80,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "circle"
+                },
+                size: {
+                  value: { min: 1, max: 3 },
+                },
+              },
+              detectRetina: true,
+            }}
+        />
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <form action="">
+            <div className="input-field">
+              <input
+                className="effect"
+                type="text"
+                placeholder="Username"
+                onChange={(event) => {
+                  setUsername(event.target.value);
+                }}
+                required
+              />
+              <span class="focus-border"></span>
+            </div>
+            <div className="input-field">
+              <input
+                className="effect"
+                type="text"
+                placeholder="Room Id"
+                onChange={(event) => {
+                  setRoom(event.target.value);
+                }}
+                onKeyPress={(event) => {
+                  event.key === "Enter" && joinRoom();
+                }}
+                required
+              />
+              <span class="focus-border"></span>
+            </div>
+          </form>
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+          <div>
+            <Chat socket={socket} username={username} room={room} />
+          </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
