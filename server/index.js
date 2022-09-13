b2aa43a1dbb7873a0ff35ae5377a68210ee48384
ArshvirSk <arshvirsk26@gmail.com>
2022-09-13 21:18:35 +0530

    fixing problem7

--- server/index.js
+++ server/index.js
@@ -7,11 +7,11 @@ app.use(cors());
 
 const server = http.createServer(app);
 
-const io = new Server(http, {
+const io = new Server(server, {
   cors: {
     // origin: "*",
-    // origin: "https://salty-retreat-48240.herokuapp.com",
-    origin: "http://localhost:3000", 
+    origin: "https://salty-retreat-48240.herokuapp.com",
+    // origin: "http://localhost:3000", 
     methods: ["GET", "POST", "OPTIONS"],
   },
 });
