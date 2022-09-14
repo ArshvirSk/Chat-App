6b47106aef48a286af3e40c40c5d100116a3970f
ArshvirSk <arshvirsk26@gmail.com>
2022-09-15 00:33:32 +0530

    Server Commit 3

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-app.use(cors());
+// app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
