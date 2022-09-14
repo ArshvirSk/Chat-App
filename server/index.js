86328f47591fa49136de60d6eb7e8a6845af13fe
ArshvirSk <arshvirsk26@gmail.com>
2022-09-14 21:15:39 +0530

    server 2

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.use(cors());
 //   },
 // });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // server.listen(PORT, () => {
 // // server.listen(3001, () => {
