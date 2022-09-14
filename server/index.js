0804785976a0cf7afb0578aebccfaee68c1c67ca
ArshvirSk <arshvirsk26@gmail.com>
2022-09-14 22:07:05 +0530

    server 5

--- server/index.js
+++ server/index.js
@@ -1,75 +1,76 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser')
-const http = require("http");
+// const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
 app.use(cors());
 
-// const server = http.createServer(app);
-
-// const io = new Server(server, {
-//   cors: {
-//     // origin: "*",
-//     // origin: "https://salty-retreat-48240.herokuapp.com",
-//     origin: "http://localhost:3001",
-//     methods: ["GET", "POST"],
-//   },
-// });
-
 const PORT = process.env.PORT || 3000;
 
-// server.listen(PORT, () => {
-// // server.listen(3001, () => {
-//   io.on("connection", socket => {
-//     console.log("SERVER RUNNING");
-//     console.log("ok");
-//     console.log(socket.id);
-//     // io.in(roomID).emit()
-//     socket.emit("WELCOME_MESSAGE", ` ${socket.id} welcome!! `);
-//   });
-// });
+const server = app.listen(PORT);
+// const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+    // origin: "*",
+    // origin: "https://salty-retreat-48240.herokuapp.com",
+    origin: "http://localhost:3001",
+    methods: ["GET", "POST"],
+  },
+});
 
-// io.on("connection", (socket) => {
-//   console.log(`User Connected: ${socket.id}`);
 
-//   socket.on("join_room", (data) => {
-//     socket.join(data);
-//     console.log(`User with ID: ${socket.id} joined room: ${data}`);
-//   });
+server.listen(PORT, () => {
+// server.listen(3001, () => {
+  io.on("connection", socket => {
+    console.log("SERVER RUNNING");
+    console.log("ok");
+    console.log(socket.id);
+    // io.in(roomID).emit()
+    socket.emit("WELCOME_MESSAGE", ` ${socket.id} welcome!! `);
+  });
+});
 
-//   socket.on("send_message", (data) => {
-//     socket.to(data.room).emit("receive_message", data);
-//   });
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-//   socket.on("disconnect", () => {
-//     console.log("User Disconnected", socket.id);
-//   });
-// });
+  socket.on("join_room", (data) => {
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    });
 
-// if (process.env.NODE_ENV === 'production') {
-//   // Exprees will serve up production assets
-//   app.use(express.static('client/build'));
+  socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message", data);
+    });
+  
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+});
 
-//   // Express serve up index.html file if it doesn't recognize route
-//   const path = require('path');
-//   app.get('*', (req, res) => {
-//     res.send('hello world!!');
-//   });
-// };
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+  app.use(express.static('client/build'));
 
-app.use( bodyParser.json() );
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+      res.send('hello world!!');
+  });
+};
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+// app.use( bodyParser.json() );
 
-app.use(cors())
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
 
-app.get('/', (req, res)=>{
-  res.send("Welcome to your server")
-})
+// app.use(cors())
 
-app.listen(PORT, ()=>{
-  console.log(`Server is runing on port ${PORT}`)
-})
+// app.get('/', (req, res)=>{
+//   res.send("Welcome to your server")
+// })
+
+// app.listen(PORT, ()=>{
+//   console.log(`Server is runing on port ${PORT}`)
+// })
